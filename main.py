2158053b7c9d4b1680e44443834192f148b79abb
Drake Samuel <817144@stma.k12.mn.us>
2021-04-01 09:58:56 -0500

    test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if (x > y)
+        return True
+
+def lessThan(x, y):
+    elif (x < y)
+        return True
+
+def equalTo(x, y):
+    return
+
+def greaterOrEqual(x, y):
+    return
+
+def lessOrEqual(x, y):
+    return
